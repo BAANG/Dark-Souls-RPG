02426e4716b514c90a12ad98077cf5994abd9b4f
BAANG <chcelestino@gmail.com>
2019-05-27 14:05:46 -0700

    Create character sheets

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,14 @@ var charArray;
 
 // Character Sheets & Constructor (using object protoypes)
 
-function createChar (charName, healthPoints, atkPwr, counterAtk) {
+function character(charName, healthPoints, atkPwr, counterAtk) {
     this.name = charName;
     this.hp = healthPoints;
     this.ap = atkPwr;
     this.cp = counterAtk;
-}
+};
+
+var asagi = new character("Asagi, Sniper of Demons", 100, 30, 15);
+var bloodis = new character("Bloodis, Great Demon Fist", 250, 10, 5);
+var laharl = new character("Laharl, Demon Prince", 120, 25, 15);
+var prinny = new character("Prinny, Worthless Soul", 500, 1, 50);
