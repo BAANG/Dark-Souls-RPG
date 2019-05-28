a77f9518d37c459ad0aaf3afd12663bce3696f72
BAANG <chcelestino@gmail.com>
2019-05-27 19:31:25 -0700

    Link character buttons to character objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,8 @@
 var playerChar;
 var enemyChar;
 var charArray = [];
+var charPicked = false;
+var enemyPicked = false;
 
 // Character Sheets & Constructor (using object protoypes)
 
@@ -12,22 +14,30 @@ function character(charName, healthPoints, atkPwr, counterAtk, charSprite) {
     this.hp = healthPoints;
     this.ap = atkPwr;
     this.cp = counterAtk;
-    this.sprite = charSprite;
+    this.src = charSprite;
 };
 
-var asagi = new character("Asagi, Sniper of Demons", 100, 30, 15, "../images/asagi.gif");
-var bloodis = new character("Bloodis, Great Demon Fist", 250, 10, 5, "../images/bloodis.gif");
-var laharl = new character("Laharl, Demon Prince", 120, 25, 15, "../images/laharl.gif");
-var prinny = new character("Prinny, Worthless Soul", 500, 1, 50, "../images/prinny.gif");
+var asagi = new character("Asagi, Sniper of Demons", 100, 30, 15, "assets/images/asagi.gif");
+var bloodis = new character("Bloodis, Great Demon Fist", 250, 10, 5, "assets/images/bloodis.gif");
+var laharl = new character("Laharl, Demon Prince", 120, 25, 15, "assets/images/laharl.gif");
+var prinny = new character("Prinny, Worthless Soul", 500, 1, 50, "assets/images/prinny.gif");
 
-charArray.push(asagi, bloodis, laharl, prinny)
 console.log(charArray)
+
+// Load Character Information
+
+$("#asagi").attr(asagi)
+$("#bloodis").attr(bloodis)
+$("#laharl").attr(laharl)
+$("#prinny").attr(prinny)
 
 // On Page Load
 
 $(document).ready(function () {
 
-    $(document).on("click", "#charBtn", function() {
+    // Event Listener (Click Character Button)
+
+    $(".charBtn").on("click", function() {
         console.log("This is a character button.")
     })
 });
