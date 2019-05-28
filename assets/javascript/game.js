c7bca53fb223c841dca297a5e3b2fa3d12c4f271
BAANG <chcelestino@gmail.com>
2019-05-28 00:38:02 -0700

    Animate in character cut in images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,23 +8,25 @@ var hpArray = [];
 var apArray = [];
 var cpArray = [];
 var imgArray = [];
+var cutArray = [];
 var charPicked = false;
 var enemyPicked = false;
 
 // Character Sheets & Constructor (using object protoypes)
 
-function character(charName, healthPoints, atkPwr, counterAtk, charSprite) {
+function character(charName, healthPoints, atkPwr, counterAtk, charSprite, cutPortrait) {
     this.name = charName;
     this.hp = healthPoints;
     this.ap = atkPwr;
     this.cp = counterAtk;
     this.src = charSprite;
+    this.cut = cutPortrait;
 };
 
-var asagi = new character("Asagi", 100, 30, 15, "assets/images/asagi.gif");
-var bloodis = new character("Bloodis", 250, 10, 5, "assets/images/bloodis.gif");
-var laharl = new character("Laharl", 120, 25, 15, "assets/images/laharl.gif");
-var prinny = new character("Prinny", 500, 1, 50, "assets/images/prinny.gif");
+var asagi = new character("Asagi", 100, 30, 15, "assets/images/asagi.gif", "assets/images/asagicut.png");
+var bloodis = new character("Bloodis", 250, 10, 5, "assets/images/bloodis.gif", "assets/images/bloodiscut.png");
+var laharl = new character("Laharl", 120, 25, 15, "assets/images/laharl.gif", "assets/images/laharlcut.png");
+var prinny = new character("Prinny", 500, 1, 50, "assets/images/prinny.gif", "assets/images/prinnycut.png");
 
 console.log(charArray)
 
@@ -44,6 +46,10 @@ spawnChar = function() {
         cp: cpArray[i],
         src: imgArray[i]
     });
+
+    $("#playerCut").attr("src", cutArray[i]);
+
+    $("#playerCut").addClass("enter");
     
     $("#playerHealth").attr({
         value: hpArray[i],
@@ -57,6 +63,7 @@ spawnChar = function() {
     apArray.splice(i, 1);
     cpArray.splice(i, 1);
     imgArray.splice(i, 1);
+    cutArray.splice(i, 1);
 }
 
 spawnEnemy = function () {
@@ -71,8 +78,12 @@ spawnEnemy = function () {
         hp: hpArray[i],
         ap: apArray[i],
         cp: cpArray[i],
-        src: imgArray[i]
+        src: imgArray[i],
     });
+
+    $("#enemyCut").attr("src", cutArray[i]);
+    
+    $("#enemyCut").addClass("enter");
 
     $("#enemyHealth").attr({
         value: hpArray[i],
@@ -86,6 +97,7 @@ spawnEnemy = function () {
     apArray.splice(i, 1);
     cpArray.splice(i, 1);
     imgArray.splice(i, 1);
+    cutArray.splice(i, 1);
 }
 
 // Load Character Information
@@ -104,6 +116,7 @@ hpArray.push(asagi.hp, bloodis.hp, laharl.hp, prinny.hp)
 apArray.push(asagi.ap, bloodis.ap, laharl.ap, prinny.ap)
 cpArray.push(asagi.cp, bloodis.cp, laharl.cp, prinny.cp)
 imgArray.push(asagi.src, bloodis.src, laharl.src, prinny.src)
+cutArray.push(asagi.cut, bloodis.cut, laharl.cut, prinny.cut)
 
     // Event Listener (Click Character Button)
     
